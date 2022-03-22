ec0afaad7a95a4a44af7585891e15d5f9ae80c7c
CGranzell <91188714+CGranzell@users.noreply.github.com>
2022-03-22 12:55:28 +0100

    fixat media queries och nav-bar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 // Hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+const links = document.querySelectorAll(".nav-menu li");
+console.log(links);
 hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("active");
 });
+
+for (let link of links) {
+  console.log(link);
+  link.addEventListener("click", function (e) {
+    const theClickedLink = e.target;
+    console.log(theClickedLink);
+    if (theClickedLink) {
+      navMenu.classList.toggle("active");
+    }
+  });
+}
